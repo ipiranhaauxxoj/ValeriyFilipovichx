db6f285fe189c5ae04d163bfde84008886b2ce75
ipiranhaauxxoj <ran15967@126.com>
2021-04-24 13:38:08 +0800

    Add PZ01

---
+++ МДК.03.02/Практические работы/ПЗ_01.Установка JDKJRE. Настройка параметров среды/SearchHypotenuse/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        double a = 3;
+        double b = 4;
+        double c;
+
+        c = Math.sqrt (a*a + b*b);
+        System.out.println ("c = "+ c);
+    }
+}
